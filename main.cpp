e85f2b63be5cecd1e7b8c44ff23e29fdbf9b1a26
Vijay Tiwari <rsvijaytiwari@gmail.com>
2023-07-28 06:35:58 +0530

    int user input and print user id.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
 using namespace std;
-/*
- * C++ namespace specify the default module which we
- * are going to use by our program for example if you want
- * to use cout then we write std::cout and also mentioned
- * the package or module name if we declare namespace with
- * model std or standard, and we can directly write cout
- * which is totally fine.
- */
+
 int main(){
-    cout << "Hello World" << endl;
+    int number;
+    cin >> number; // getting input from user
+    printf("Your ID is %d\n", number + 3);
     return 0;
 }
