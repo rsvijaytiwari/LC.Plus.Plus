59531765234f33f5a43039d1f1dd206f197cc9b4
Vijay Tiwari <rsvijaytiwari@gmail.com>
2023-07-28 06:53:53 +0530

    getting user input using getline

--- main.cpp
+++ main.cpp
@@ -1,21 +1,13 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-int main(){
-    /*
-     * Primitives Data Type
-     * Boolean bool
-     * Character char
-     * Integer int
-     * Floating point float
-     * Double Floating Point double
-     * Valueless void
-     */
-    int number; // declaration
-//    number = 0; // variable initialization
-    // int number = 0; for one declaration
+int main() {
+    string my_color;
+    cout << "Enter Your Favorite Color: \n";
+    getline(cin, my_color); // Getting input using getline
+    cout << "Hey " << my_color << " is my favorite too";
 
-    cin >> number; // getting input from user
-    printf("Your ID is %d\n", number + 3);
     return 0;
 }
