f40a5b04068680d319cec6e7bbfb979bf7d70497
Vijay Tiwari <rsvijaytiwari@gmail.com>
2023-07-28 06:10:28 +0530

    initial

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+using namespace std;
+int main(){ // block of code and function
+    puts("Click on Button"); // statement
+    puts("create a new player");
+    puts("add life to player");
+    return 0; // return an exit with 0 code
+}
+/*
+ * This is multiline comment
+ */
+
+// this is for your reference.
+void nothing(){
+    puts("Hello World");
+}
+char iamchar(){
+    return 'r';
+}
