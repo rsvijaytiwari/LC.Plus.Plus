54f48284989881b8e22d64b7edb2bda572f69f9c
Vijay Tiwari <rsvijaytiwari@gmail.com>
2023-07-28 06:50:17 +0530

    studied primitives data type

--- main.cpp
+++ main.cpp
@@ -3,18 +3,18 @@ using namespace std;
 
 int main(){
     /*
-     * Identifiers
-     * Upper case and lower case letters: timTimmy, timtimmy
-     * Numbers-0-9 //we can insert number between name and end of name and not in start.
-     * Underscore: _
-     * Conflict with reserved keywords like int, friend, if , inline
-     * Non latin support
-     * dont_go_beyond_31_characters
-     * _at start means private
-     * __means system keyword
-     *
+     * Primitives Data Type
+     * Boolean bool
+     * Character char
+     * Integer int
+     * Floating point float
+     * Double Floating Point double
+     * Valueless void
      */
-    int number;
+    int number; // declaration
+//    number = 0; // variable initialization
+    // int number = 0; for one declaration
+
     cin >> number; // getting input from user
     printf("Your ID is %d\n", number + 3);
     return 0;
