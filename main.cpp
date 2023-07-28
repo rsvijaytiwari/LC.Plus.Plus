f78132bb5ba39e8f7aba23483c0468c9f6381be2
Vijay Tiwari <rsvijaytiwari@gmail.com>
2023-07-28 06:54:23 +0530

    added endl.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
     string my_color;
     cout << "Enter Your Favorite Color: \n";
     getline(cin, my_color); // Getting input using getline
-    cout << "Hey " << my_color << " is my favorite too";
+    cout << "Hey " << my_color << " is my favorite too" << endl;
 
     return 0;
 }
