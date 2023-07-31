8a734af30ced56560b59dffb551c140a42dacbd2
Vijay Tiwari <rsvijaytiwari@gmail.com>
2023-07-31 08:09:03 +0530

    studied c++ integer with convert mechanism like hex and binary.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
 
 int main() {
-    int integer_array[4] = {1, 2, 3, 4};
-    cout << integer_array[0] << endl;
+    // 1 byte is of 8bit
+    printf("Size of this data type ids %ld bits\n", sizeof(short int) * 8);
+    printf("Size of this data type ids %ld bits\n", sizeof(int) * 8);
+    printf("Size of this data type ids %ld bits\n", sizeof(long int) * 8);
+    printf("Size of this data type ids %ld bits\n", sizeof(long long int) * 8);
+    cout << endl;
+    printf("Size of this data type ids %ld bits\n", sizeof(unsigned short int) * 8);
+    printf("Size of this data type ids %ld bits\n", sizeof(unsigned int) * 8);
+    printf("Size of this data type ids %ld bits\n", sizeof(unsigned long int) * 8);
+    printf("Size of this data type ids %ld bits\n", sizeof(unsigned long long int) * 8);
+    cout << endl;
+    printf("Size of this data type ids %ld bits\n", sizeof(int16_t) * 8);
+    printf("Size of this data type ids %ld bits\n", sizeof(int32_t) * 8);
 
-    int another_array[4];
-    another_array[0] = 9;
-    another_array[1] = 19;
-
-    cout << another_array[0] << endl;
-    cout << another_array[1] << endl; // default is zero
-    int *ap = another_array;
-    *ap = 240;
-    ap = ap + 1;
-    *ap = 250;
-    cout << another_array[0] << endl;
-    cout << another_array[1] << endl;
+    printf("Fun value of %d\n", 0x16);
+    printf("Fun value of %d\n", 0b00010110);
 
     return 0;
 }
