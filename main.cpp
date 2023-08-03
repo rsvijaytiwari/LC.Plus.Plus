aa84b1afbd778b457cb6bbae2f85bb933a9134a8
Vijay Tiwari <rsvijaytiwari@gmail.com>
2023-08-03 09:07:40 +0530

    studied float, double, long double sizes and precision

--- main.cpp
+++ main.cpp
@@ -3,15 +3,17 @@
 using namespace std;
 
 int main() {
-    switch (1) {
-        case 1:
-            puts("It one");
-            break;
-        case 2:
-            puts("It two");
-            break;
-        default:
-            puts("It default");
+    printf("Float size is %ld bits \n", sizeof(long) * 8);
+    printf("Float size is %ld bits \n", sizeof(double) * 8);
+    printf("Float size is %ld bits \n", sizeof(long double) * 8);
+
+    float my_p_value = 20.2 + 20.2;
+
+    if (my_p_value == 40.40002){
+        puts("You got it right");
+    }else {
+        puts("Something");
     }
+    printf("My P Value is: %1.30f in float\n", my_p_value);
     return 0;
 }
